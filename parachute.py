ee4ca6030c4f97a946b5ac24504385b7b8d59308
GKrenk <97206108+GKrenk@users.noreply.github.com>
2022-02-01 14:27:19 -0700

    Add files via upload

---
+++ parachute.py
@@ -0,0 +1,41 @@
+x = input("Press enter")
+
+#class Parachute:
+
+#    def __init__(self):
+#        self.life = 4
+
+#    def jump(self):
+
+#        if self.life == 4:
+print("  _____")
+print(" /_____\\")
+print(" \     /")
+print("  \   /")
+print("    0")
+print("   /|\\")
+print("   / \\")
+print("\n^^^^^^^^^")
+#        if self.life == 3:
+#            print(" /_____\\")
+#            print(" \     /")
+#            print("  \   /")
+#            print("    0")
+#            print("   /|\\")
+#            print("   / \\")
+#        if self.life == 2:
+#            print(" \     /")
+#            print("  \   /")
+#            print("    0")
+#            print("   /|\\")
+#            print("   / \\")
+#        if self.life == 1:
+#            print("  \   /")
+#            print("    0")
+#            print("   /|\\")
+#            print("   / \\")
+#        if self.life == 0:
+#            print("    X")
+#            print("   /|\\")
+#            print("   / \\")
+
