e08e90579abf874841e7a1f5d066048e4edb3485
Gkrenk <you@example.com>
2022-02-03 15:09:58 -0700

    Update

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -1,3 +1,5 @@
+from game.director import Director
+
 class Jumper:
     """The person playing the game (guessing). 
     
@@ -13,4 +15,43 @@ class Jumper:
         Args:
             self (Jumper): An instance of Jumper.
         """
-        pass
+        self.life = 4
+
+    def jump(self):
+
+        if self.life == 4:
+            print("  _____")
+            print(" /_____\\")
+            print(" \     /")
+            print("  \   /")
+            print("    0")
+            print("   /|\\")
+            print("   / \\")
+            print("\n^^^^^^^^^")
+        if self.life == 3:
+            print(" /_____\\")
+            print(" \     /")
+            print("  \   /")
+            print("    0")
+            print("   /|\\")
+            print("   / \\")
+            print("\n^^^^^^^^^")
+        if self.life == 2:
+            print(" \     /")
+            print("  \   /")
+            print("    0")
+            print("   /|\\")
+            print("   / \\")
+            print("\n^^^^^^^^^")
+        if self.life == 1:
+            print("  \   /")
+            print("    0")
+            print("   /|\\")
+            print("   / \\")
+            print("\n^^^^^^^^^")
+        if self.life == 0:
+            print("    X")
+            print("   /|\\")
+            print("   / \\")
+            print("\n^^^^^^^^^")
+            Director.is_playing = False
